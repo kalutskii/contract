a9e2c504e6efc2cda4f5aefcd93fac01ad4c07ff
kalutskii <ilyakalutskii@gmail.com>
2025-11-18 00:10:36 +0400

    Fix schema URL username

--- src/config/config.defaults.ts
+++ src/config/config.defaults.ts
@@ -1,7 +1,7 @@
 import { Config } from './config.schemas.js';
 
 export const defaultConfig: Config = {
-  $schema: 'https://raw.githubusercontent.com/kaluckii/contract/refs/heads/main/static/contract.schema.json',
+  $schema: 'https://raw.githubusercontent.com/kalutskii/contract/refs/heads/main/static/contract.schema.json',
   appName: 'your-app',
   externalSources: [],
   contracts: [],
